06330ebe88cde0100c4f8cb60db5bc75ae75252d
ChristoferMendes <christoferluizdsm@gmail.com>
2023-04-26 08:46:43 -0300

    fixed routes

--- src/modules/characters/routes/index.ts
+++ src/modules/characters/routes/index.ts
@@ -3,6 +3,5 @@ import { characterDiplomat } from "../diplomat";
 
 export const characterRouter = Router();
 
-// characterRouter.get('/', characterDiplomat.getAll)
 characterRouter.get('/', characterDiplomat.getAllByUserId)
 characterRouter.post('/', characterDiplomat.create)
